fdb41ea703f02322014d9c399bef83f078bf42fb
unknown <santiagoryan788@gmail.com>
2023-12-11 14:42:54 +0800

    new commit

---
+++ bma_gsm/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace bma_gsm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.server_log = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.websites = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.PortName = new System.Windows.Forms.ComboBox();
+            this.connectPort = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(281, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(627, 335);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // server_log
+            // 
+            this.server_log.Location = new System.Drawing.Point(13, 29);
+            this.server_log.Name = "server_log";
+            this.server_log.ReadOnly = true;
+            this.server_log.Size = new System.Drawing.Size(263, 255);
+            this.server_log.TabIndex = 1;
+            this.server_log.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Connected:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Server Status:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // websites
+            // 
+            this.websites.AutoSize = true;
+            this.websites.Location = new System.Drawing.Point(340, 13);
+            this.websites.Name = "websites";
+            this.websites.Size = new System.Drawing.Size(51, 13);
+            this.websites.TabIndex = 5;
+            this.websites.Text = "Websites";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "GSM PORTNAME:";
+            // 
+            // PortName
+            // 
+            this.PortName.FormattingEnabled = true;
+            this.PortName.Location = new System.Drawing.Point(12, 343);
+            this.PortName.Name = "PortName";
+            this.PortName.Size = new System.Drawing.Size(130, 21);
+            this.PortName.TabIndex = 8;
+            // 
+            // connectPort
+            // 
+            this.connectPort.Location = new System.Drawing.Point(148, 341);
+            this.connectPort.Name = "connectPort";
+            this.connectPort.Size = new System.Drawing.Size(75, 23);
+            this.connectPort.TabIndex = 9;
+            this.connectPort.Text = "Connect";
+            this.connectPort.UseVisualStyleBackColor = true;
+            this.connectPort.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 376);
+            this.Controls.Add(this.connectPort);
+            this.Controls.Add(this.PortName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.websites);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.server_log);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "BMA GSM APPLICATION";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox server_log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label websites;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox PortName;
+        private System.Windows.Forms.Button connectPort;
+    }
+}
+
---
+++ bma_gsm/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Data;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace bma_gsm
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private HttpListener httpListener;
+        private Thread listenerThread;
+        private DataTable dataTable;
+        private System.Windows.Forms.Timer refreshTimer;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize SerialPort
+            serialPort = new SerialPort();
+            serialPort.BaudRate = 9600; // Change this to your baud rate
+            serialPort.DataReceived += SerialPort_DataReceived;
+
+            // Populate ComboBox with available ports
+            string[] ports = SerialPort.GetPortNames();
+            PortName.Items.AddRange(ports);
+
+            if (ports.Length > 0)
+            {
+                // Set default port
+                PortName.SelectedIndex = 0;
+                serialPort.PortName = ports[0];
+            }
+
+            refreshTimer = new System.Windows.Forms.Timer();
+            refreshTimer.Interval = 5000;
+            refreshTimer.Tick += new EventHandler(timer1_Tick); 
+            refreshTimer.Start();
+            StartListener();
+
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+            buttonColumn.Name = "Properties";
+            buttonColumn.Text = "Resend";
+            buttonColumn.UseColumnTextForButtonValue = true;
+            dataGridView1.Columns.Add(buttonColumn);
+
+            dataTable = new DataTable();
+            dataTable.Columns.Add("Host", typeof(string));
+            dataTable.Columns.Add("Client Number", typeof(string));
+            dataTable.Columns.Add("Status", typeof(string));
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // This event will be called when data is received
+            SerialPort sp = (SerialPort)sender;
+            string data = sp.ReadExisting(); // Read the available data
+
+            // Update the ComboBox with received data
+            Invoke(new Action(() =>
+            {
+                PortName.Items.Add(data);
+            }));
+        }
+
+        private void StartListener()
+        {
+            try
+            {
+                httpListener = new HttpListener();
+                httpListener.Prefixes.Add("http://localhost:9090/");//ito ung host
+                httpListener.Start();
+
+                // Start a new thread to handle incoming requests
+                listenerThread = new Thread(HandleRequests);
+                listenerThread.Start();
+                LogMessage("Server started.");
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"Error starting server: {ex.Message}");
+            }
+        }
+
+        private void HandleRequests()
+        {
+            while (httpListener.IsListening)
+            {
+                try
+                {
+                    HttpListenerContext context = httpListener.GetContext();
+                    ProcessRequest(context);
+                }
+                catch (Exception ex)
+                {
+                    LogMessage($"Error handling request: {ex.Message}");
+                }
+            }
+        }
+
+        private void ProcessRequest(HttpListenerContext context)
+        {
+            try
+            {
+                HttpListenerRequest request = context.Request;
+                string requestData = new System.IO.StreamReader(request.InputStream, request.ContentEncoding).ReadToEnd();
+
+                DisplayRequestInRichTextBox(requestData);
+
+                HttpListenerResponse response = context.Response;
+                string responseString = "<html><body><h1>Hello from LocalHost!</h1></body></html>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+                response.ContentType = "text/html";
+                response.ContentLength64 = buffer.Length;
+
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"Error processing request: {ex.Message}");
+            }
+        }
+        private void DisplayRequestInRichTextBox(string requestData)
+        {
+            string[] requestDataParts = requestData.Split(',');
+
+            // Remove specific characters from each element of the array
+            for (int i = 0; i < requestDataParts.Length; i++)
+            {
+                requestDataParts[i] = requestDataParts[i].Replace("\"", "").Replace("[", "").Replace("]", "");
+            }
+
+            if (requestDataParts.Length >= 2) // kung ilang data pinapasa
+            {
+                DataRow newRow = dataTable.NewRow();
+                newRow["Host"] = requestDataParts[0]; 
+                newRow["Client Number"] = requestDataParts[1];
+
+                if (requestDataParts[1] == "" || PortName.Text == "")
+                {
+                    MessageBox.Show("Please Connect Port");
+                }
+                else
+                {
+                    SerialPort sp = new SerialPort();
+                    sp.PortName = PortName.Text;
+                    sp.Open();
+                    sp.WriteLine("AT" + Environment.NewLine);
+                    Thread.Sleep(100);
+                    sp.WriteLine("AT+CMGF=1" + Environment.NewLine);
+                    Thread.Sleep(100);
+                    sp.WriteLine("AT+CSCS=\"GSM\"" + Environment.NewLine);
+                    Thread.Sleep(100);
+                    sp.WriteLine("AT+CMGF\"" + requestDataParts[1] + "\"" + Environment.NewLine);
+                    Thread.Sleep(100);
+                    sp.WriteLine("SMS MESSAGE FROM BALIWAG MARITIME ACADEMY" + Environment.NewLine);
+                    Thread.Sleep(100);
+                    sp.Write(new byte[] { 26 }, 0, 1);
+                    Thread.Sleep(100);
+                    var response = sp.ReadExisting();
+                    if (response.Contains("ERROR"))
+                    {
+                        newRow["Status"] = "Failed";
+                    }
+                    else
+                    {
+                        newRow["Status"] = "Success";
+                    }
+                    sp.Close();
+                }
+                dataTable.Rows.Add(newRow);
+            }
+            if (dataGridView1.InvokeRequired)
+            {
+                dataGridView1.Invoke((MethodInvoker)delegate {
+                    dataGridView1.DataSource = null; 
+                    dataGridView1.DataSource = dataTable; 
+                });
+            }
+            else
+            {
+                dataGridView1.DataSource = null; 
+                dataGridView1.DataSource = dataTable; 
+            }
+        }
+        private void LogMessage(string message)
+        {
+            if (server_log.InvokeRequired)
+            {
+                server_log.Invoke((MethodInvoker)delegate { server_log.AppendText($"{message}\n"); });
+            }
+            else
+            {
+                server_log.AppendText($"{message}\n");
+            }
+        }
+
+        private async Task ReceiveDataFromServer()
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync("http://127.0.0.1:8000/connection"); //ito ung laravel connection
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    websites.Text = responseBody; // ito ung ilang naka connect bali kung mag add another add getAsync
+                    //response then "  int connections = Convert.ToInt32(responseBody) + Convert.ToInt32(responseBody);
+                    //websites.Text = Convert.ToString(connections);
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                websites.Text = "0"; //pag alang nakaconnect then 0 connection
+                server_log.Text = Convert.ToString(ex); //kung my error naganap papakita sa serverlog
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            httpListener?.Stop();
+            httpListener?.Close();
+            StartListener();
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            await ReceiveDataFromServer(); //timer every 5sec tinitignan kung my nakaconnect sa laravel
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Connect to the selected port
+            if (PortName.Text == "")
+            {
+                MessageBox.Show("Select PortName");
+            }
+            else
+            {
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.PortName = PortName.SelectedItem.ToString();
+
+                    try
+                    {
+                        serialPort.Open();
+                        connectPort.Enabled = false; // Disable the connect button after successful connection
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error opening serial port: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
